be9492a9452fd6fa280302656a3936b5df3a6511
IrenaSte <irena.stepanovska@mir.uist.edu.mk>
2016-07-26 15:46:59 +0200

    Update 10-install-yad.sh

--- puphpet/files/exec-once/10-install-yad.sh
+++ puphpet/files/exec-once/10-install-yad.sh
@@ -8,9 +8,9 @@ cd /etc/yad/
 git clone https://github.com/AOE-T3Rookies-2016/YAD.git deploy
 
 mkdir -p /etc/yad/t3rookies/typo3/
-
+cd /etc/yad/t3rookies/typo3/
 wget https://raw.githubusercontent.com/AOE-T3Rookies-2016/devbox/master/share/etc/yad/t3rookies/typo3/devbox.sh
 
-cd /var/share/usr/
+cd /usr/local/bin/
 wget https://raw.githubusercontent.com/AOE-T3Rookies-2016/devbox/master/share/usr/local/bin/yad_t3rookies_typo3_devbox
 chmod +x yad_t3rookies_typo3_devbox
